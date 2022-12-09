1623d7ca8e5b167a6f9566c346a19a4dd06bd2cc
bmxpc7 <laser.hugo@hotmail.com>
2022-12-09 08:53:02 -0600

    Se sube styled en login

---
+++ src/page/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Title, Wrapper } from "./login-styled"
+
+const Login = () => {
+    return (
+        <Wrapper>
+            <Title />
+        </Wrapper>
+    )
+}
+
+export default Login
